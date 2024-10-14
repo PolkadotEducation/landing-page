f203e455878204b2409a5a1fd6b4b1d9caa5d0c6
Ewerton Keiji Onga <ewerton_onga@hotmail.com>
2024-10-14 20:03:12 -0300

    Update icons and layout nits (#3)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,38 @@
-import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  locales: ['en', 'es', 'pt'],
- 
-  defaultLocale: 'en'
-});
- 
+import { NextRequest, NextResponse } from 'next/server';
+import { match } from '@formatjs/intl-localematcher';
+
+const locales = ['en', 'es', 'pt'];
+
+const validPaths = ['/'];
+
+function getLocale(req: NextRequest): string {
+  const cookieLocale = req.cookies.get('NEXT_LOCALE')?.value;
+  const headerLocale = req.headers.get('accept-language')?.split(',')[0];
+  
+  return cookieLocale || match([headerLocale || ''], locales, 'en') || 'en';
+}
+
+function isValidPage(pathname: string): boolean {
+  return validPaths.includes(pathname);
+}
+
+export function middleware(req: NextRequest) {
+  const locale = getLocale(req);
+  
+  const res = NextResponse.next();
+  res.cookies.set('NEXT_LOCALE', locale);
+
+  const { pathname } = req.nextUrl;
+
+  if (!isValidPage(pathname)) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
+  return res;
+}
+
 export const config = {
-  matcher: ['/', '/(es|en|pt)/:path*']
+  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
 };
